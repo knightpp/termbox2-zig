09b9f6d668f31096ee7fbda5177cca59230e7887
Danylo Kondratiev <knightpp@proton.me>
2024-06-07 14:37:24 +0300

    Fix options

--- build.zig
+++ build.zig
@@ -26,7 +26,12 @@ pub fn build(b: *std.Build) void {
     lib.installHeader(termbox2_c.path("termbox2.h"), "termbox2.h");
     b.installArtifact(lib);
 
-    const module = b.addModule("termbox2", .{ .root_source_file = lib.root_module.root_source_file });
+    const module = b.addModule("termbox2", .{
+        .root_source_file = lib.root_module.root_source_file,
+        .target = target,
+        .optimize = optimize,
+    });
+    module.addOptions("build_options", build_options);
     module.addIncludePath(termbox2_c.path(""));
 
     const lib_unit_tests = b.addTest(.{
