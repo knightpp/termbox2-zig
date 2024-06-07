e4d50206a8c1535b2f9102319c67eb7f08290513
Danylo Kondratiev <knightpp@proton.me>
2024-06-07 14:25:08 +0300

    Init

---
+++ build.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const build_options = b.addOptions();
+    build_options.addOption(i32, "attr_w", b.option(i32, "attr_w", "integer width of fg and bg attributes") orelse 0);
+    build_options.addOption(bool, "egc", b.option(bool, "egc", "enable extended grapheme cluster support") orelse false);
+    build_options.addOption(usize, "printf_buf", b.option(usize, "printf_buf", "buffer size for printf operations") orelse 0);
+    build_options.addOption(usize, "read_buf", b.option(usize, "read_buf", "buffer size for tty reads") orelse 0);
+
+    const termbox2_c = b.dependency("termbox2", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "termbox2-zig",
+        .root_source_file = b.path("src/termbox2.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.root_module.addOptions("build_options", build_options);
+    lib.linkLibC();
+    lib.installHeader(termbox2_c.path("termbox2.h"), "termbox2.h");
+    b.installArtifact(lib);
+
+    const module = b.addModule("termbox2", .{ .root_source_file = lib.root_module.root_source_file });
+    module.addIncludePath(termbox2_c.path(""));
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lib_unit_tests.linkLibrary(lib);
+
+    const test_step = b.step("test", "Run unit tests");
+    const build_only = b.option(bool, "build", "Build tests but do not run") orelse false;
+    if (build_only) {
+        const tests_artifact = b.addInstallArtifact(lib_unit_tests, .{});
+        test_step.dependOn(&tests_artifact.step);
+    } else {
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
+
+    const cov_step = b.step("cov", "Generate coverage");
+    const cov_run = b.addSystemCommand(&.{ "kcov", "--clean", "--include-pattern=src/", "kcov-output" });
+    cov_run.addArtifactArg(lib_unit_tests);
+    cov_step.dependOn(&cov_run.step);
+
+    const lints_step = b.step("lints", "Run lints");
+    const lints = b.addFmt(.{
+        .paths = &.{ "src", "build.zig" },
+        .check = true,
+    });
+
+    lints_step.dependOn(&lints.step);
+    b.default_step.dependOn(lints_step);
+}
---
+++ src/termbox2.zig
@@ -0,0 +1,141 @@
+const opts = @import("build_options");
+pub const c = @cImport({
+    @cDefine("TB_IMPL", {});
+    if (opts.attr_w != 0) {
+        @cDefine("TB_OPT_ATTR_W", std.fmt.comptimePrint("{}", .{opts.attr_w}));
+    }
+    if (opts.egc) {
+        @cDefine("TB_OPT_EGC", {});
+    }
+    if (opts.printf_buf != 0) {
+        @cDefine("TB_OPT_PRINTF_BUF", std.fmt.comptimePrint("{}", .{opts.printf_buf}));
+    }
+    if (opts.read_buf != 0) {
+        @cDefine("TB_OPT_READ_BUF", std.fmt.comptimePrint("{}", .{opts.read_buf}));
+    }
+    @cInclude("termbox2.h");
+});
+const std = @import("std");
+
+pub const Error = error{
+    UNRECOGNIZED,
+    ERR,
+    NEED_MORE,
+    INIT_ALREADY,
+    INIT_OPEN,
+    MEM,
+    NO_EVENT,
+    NO_TERM,
+    NOT_INIT,
+    OUT_OF_BOUNDS,
+    READ,
+    RESIZE_IOCTL,
+    RESIZE_PIPE,
+    RESIZE_SIGACTION,
+    POLL,
+    TCGETATTR,
+    TCSETATTR,
+    UNSUPPORTED_TERM,
+    RESIZE_WRITE,
+    RESIZE_POLL,
+    RESIZE_READ,
+    RESIZE_SSCANF,
+    CAP_COLLISION,
+};
+
+fn parseErr(int: c_int) Error!void {
+    return switch (int) {
+        c.TB_OK => {},
+        c.TB_ERR => Error.ERR,
+        c.TB_ERR_NEED_MORE => Error.NEED_MORE,
+        c.TB_ERR_INIT_ALREADY => Error.INIT_ALREADY,
+        c.TB_ERR_INIT_OPEN => Error.INIT_OPEN,
+        c.TB_ERR_MEM => Error.MEM,
+        c.TB_ERR_NO_EVENT => Error.NO_EVENT,
+        c.TB_ERR_NO_TERM => Error.NO_TERM,
+        c.TB_ERR_NOT_INIT => Error.NOT_INIT,
+        c.TB_ERR_OUT_OF_BOUNDS => Error.OUT_OF_BOUNDS,
+        c.TB_ERR_READ => Error.READ,
+        c.TB_ERR_RESIZE_IOCTL => Error.RESIZE_IOCTL,
+        c.TB_ERR_RESIZE_PIPE => Error.RESIZE_PIPE,
+        c.TB_ERR_RESIZE_SIGACTION => Error.RESIZE_SIGACTION,
+        c.TB_ERR_POLL => Error.POLL,
+        c.TB_ERR_TCGETATTR => Error.TCGETATTR,
+        c.TB_ERR_TCSETATTR => Error.TCSETATTR,
+        c.TB_ERR_UNSUPPORTED_TERM => Error.UNSUPPORTED_TERM,
+        c.TB_ERR_RESIZE_WRITE => Error.RESIZE_WRITE,
+        c.TB_ERR_RESIZE_POLL => Error.RESIZE_POLL,
+        c.TB_ERR_RESIZE_READ => Error.RESIZE_READ,
+        c.TB_ERR_RESIZE_SSCANF => Error.RESIZE_SSCANF,
+        c.TB_ERR_CAP_COLLISION => Error.CAP_COLLISION,
+        else => {
+            if (int > 0) {
+                return;
+            }
+
+            return Error.UNRECOGNIZED;
+        },
+    };
+}
+
+pub fn init() Error!void {
+    return parseErr(c.tb_init());
+}
+pub fn shutdown() Error!void {
+    return parseErr(c.tb_shutdown());
+}
+
+pub fn width() Error!c_int {
+    const w = c.tb_width();
+    if (w > 0) {
+        return w;
+    }
+    try parseErr(w);
+    return w;
+}
+pub fn height() Error!c_int {
+    const h = c.tb_height();
+    if (h > 0) {
+        return h;
+    }
+    try parseErr(h);
+    return h;
+}
+
+pub fn clear() Error!void {
+    return parseErr(c.tb_clear());
+}
+pub fn present() Error!void {
+    return parseErr(c.tb_present());
+}
+
+pub fn set_cursor(cx: c_int, cy: c_int) Error!void {
+    return parseErr(c.tb_set_cursor(cx, cy));
+}
+pub fn hide_cursor() Error!void {
+    return parseErr(c.tb_hide_cursor());
+}
+
+pub fn set_cell(x: c_int, y: c_int, ch: u32, fg: c.uintattr_t, bg: c.uintattr_t) Error!void {
+    return parseErr(c.tb_set_cell(x, y, ch, fg, bg));
+}
+
+pub fn peek_event(event: *c.tb_event, timeout_ms: c_int) Error!void {
+    return parseErr(c.tb_peek_event(event, timeout_ms));
+}
+pub fn poll_event(event: *c.tb_event) Error!void {
+    return parseErr(c.tb_poll_event(event));
+}
+
+pub fn print(x: c_int, y: c_int, fg: c.uintattr_t, bg: c.uintattr_t, str: [:0]const u8) Error!void {
+    return parseErr(c.tb_print(x, y, fg, bg, str));
+}
+pub fn printf(alloc: std.mem.Allocator, x: c_int, y: c_int, fg: c.uintattr_t, bg: c.uintattr_t, comptime fmt: []const u8, args: anytype) !void {
+    var buf = std.ArrayList(u8).init(alloc);
+    defer buf.deinit();
+
+    try std.fmt.format(buf.writer(), fmt, args);
+    try buf.append(0);
+
+    try print(x, y, fg, bg, @ptrCast(buf.items));
+}
