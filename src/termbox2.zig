3ce860bd2894cb06f3656cb05370a838aa9a10bc
Danylo Kondratiev <knightpp@proton.me>
2024-06-08 10:35:58 +0300

    Add more zig

--- src/termbox2.zig
+++ src/termbox2.zig
@@ -43,9 +43,10 @@ pub const Error = error{
     CAP_COLLISION,
 };
 
-fn parseErr(int: c_int) Error!void {
+fn mapErr(int: c_int) Error {
+    std.debug.assert(int < 0);
+
     return switch (int) {
-        c.TB_OK => {},
         c.TB_ERR => Error.ERR,
         c.TB_ERR_NEED_MORE => Error.NEED_MORE,
         c.TB_ERR_INIT_ALREADY => Error.INIT_ALREADY,
@@ -68,15 +69,76 @@ fn parseErr(int: c_int) Error!void {
         c.TB_ERR_RESIZE_READ => Error.RESIZE_READ,
         c.TB_ERR_RESIZE_SSCANF => Error.RESIZE_SSCANF,
         c.TB_ERR_CAP_COLLISION => Error.CAP_COLLISION,
-        else => {
-            if (int > 0) {
-                return;
-            }
-
-            return Error.UNRECOGNIZED;
-        },
+        else => Error.UNRECOGNIZED,
     };
 }
+
+fn parseErr(int: c_int) Error!void {
+    if (int >= 0) {
+        return;
+    }
+
+    return mapErr(int);
+}
+
+pub const Attribute = switch (opts.attr_w) {
+    .default, .@"16" => enum(c.uintattr_t) {
+        default = c.TB_DEFAULT,
+        black = c.TB_BLACK,
+        red = c.TB_RED,
+        green = c.TB_GREEN,
+        yellow = c.TB_YELLOW,
+        blue = c.TB_BLUE,
+        magenta = c.TB_MAGENTA,
+        cyan = c.TB_CYAN,
+        white = c.TB_WHITE,
+    },
+    .@"32" => enum(c.uintattr_t) {
+        default = c.TB_DEFAULT,
+        black = c.TB_BLACK,
+        red = c.TB_RED,
+        green = c.TB_GREEN,
+        yellow = c.TB_YELLOW,
+        blue = c.TB_BLUE,
+        magenta = c.TB_MAGENTA,
+        cyan = c.TB_CYAN,
+        white = c.TB_WHITE,
+
+        bold = c.TB_BOLD,
+        underline = c.TB_UNDERLINE,
+        reverse = c.TB_REVERSE,
+        italic = c.TB_ITALIC,
+        blink = c.TB_BLINK,
+        hi_black = c.TB_HI_BLACK,
+        bright = c.TB_BRIGHT,
+        dim = c.TB_DIM,
+    },
+    .@"64" => enum(c.uintattr_t) {
+        default = c.TB_DEFAULT,
+        black = c.TB_BLACK,
+        red = c.TB_RED,
+        green = c.TB_GREEN,
+        yellow = c.TB_YELLOW,
+        blue = c.TB_BLUE,
+        magenta = c.TB_MAGENTA,
+        cyan = c.TB_CYAN,
+        white = c.TB_WHITE,
+
+        bold = c.TB_BOLD,
+        underline = c.TB_UNDERLINE,
+        reverse = c.TB_REVERSE,
+        italic = c.TB_ITALIC,
+        blink = c.TB_BLINK,
+        hi_black = c.TB_HI_BLACK,
+        bright = c.TB_BRIGHT,
+        dim = c.TB_DIM,
+
+        strikeout = c.TB_STRIKEOUT,
+        underline_2 = c.TB_UNDERLINE_2,
+        overline = c.TB_OVERLINE,
+        invisible = c.TB_INVISIBLE,
+    },
+};
 
 pub fn init() Error!void {
     return parseErr(c.tb_init());
@@ -85,21 +147,19 @@ pub fn shutdown() Error!void {
     return parseErr(c.tb_shutdown());
 }
 
-pub fn width() Error!c_int {
+pub fn width() Error!u32 {
     const w = c.tb_width();
-    if (w > 0) {
-        return w;
+    if (w < 0) {
+        return mapErr(w);
     }
-    try parseErr(w);
-    return w;
+    return std.math.cast(u32, w) orelse unreachable;
 }
-pub fn height() Error!c_int {
+pub fn height() Error!u32 {
     const h = c.tb_height();
-    if (h > 0) {
-        return h;
+    if (h < 0) {
+        return mapErr(h);
     }
-    try parseErr(h);
-    return h;
+    return std.math.cast(u32, h) orelse unreachable;
 }
 
 pub fn clear() Error!void {
@@ -109,26 +169,28 @@ pub fn present() Error!void {
     return parseErr(c.tb_present());
 }
 
-pub fn set_cursor(cx: c_int, cy: c_int) Error!void {
-    return parseErr(c.tb_set_cursor(cx, cy));
+pub fn set_cursor(cx: u32, cy: u32) Error!void {
+    return parseErr(c.tb_set_cursor(@intCast(cx), @intCast(cy)));
 }
 pub fn hide_cursor() Error!void {
     return parseErr(c.tb_hide_cursor());
 }
 
-pub fn set_cell(x: c_int, y: c_int, ch: u32, fg: c.uintattr_t, bg: c.uintattr_t) Error!void {
-    return parseErr(c.tb_set_cell(x, y, ch, fg, bg));
+pub fn set_cell(x: u32, y: u32, char: u32, fg: Attribute, bg: Attribute) Error!void {
+    return parseErr(c.tb_set_cell(@intCast(x), @intCast(y), char, @intFromEnum(fg), @intFromEnum(bg)));
 }
 
 pub fn peek_event(event: *c.tb_event, timeout_ms: c_int) Error!void {
     return parseErr(c.tb_peek_event(event, timeout_ms));
 }
-pub fn poll_event(event: *c.tb_event) Error!void {
-    return parseErr(c.tb_poll_event(event));
+pub fn poll_event() !c.tb_event {
+    var event: c.tb_event = std.mem.zeroes(c.tb_event);
+    try parseErr(c.tb_poll_event(&event));
+    return event;
 }
 
-pub fn print(x: c_int, y: c_int, fg: c.uintattr_t, bg: c.uintattr_t, str: [:0]const u8) Error!void {
-    return parseErr(c.tb_print(x, y, fg, bg, str));
+pub fn print(x: u32, y: u32, fg: Attribute, bg: Attribute, str: [*:0]const u8) Error!void {
+    return parseErr(c.tb_print(@intCast(x), @intCast(y), @intFromEnum(fg), @intFromEnum(bg), str));
 }
 pub fn printf(alloc: std.mem.Allocator, x: c_int, y: c_int, fg: c.uintattr_t, bg: c.uintattr_t, comptime fmt: []const u8, args: anytype) !void {
     var buf = std.ArrayList(u8).init(alloc);
@@ -138,4 +200,75 @@ pub fn printf(alloc: std.mem.Allocator, x: c_int, y: c_int, fg: c.uintattr_t, bg
     try buf.append(0);
 
     try print(x, y, fg, bg, @ptrCast(buf.items));
+}
+
+const testing = std.testing;
+
+test "width" {
+    try init();
+
+    const w = try width();
+    try testing.expect(w > 0);
+
+    try shutdown();
+}
+
+test "height" {
+    try init();
+
+    const h = try height();
+    try testing.expect(h > 0);
+
+    try shutdown();
+}
+
+test "clear" {
+    try init();
+
+    try clear();
+
+    try shutdown();
+}
+
+test "present" {
+    try init();
+
+    try present();
+
+    try shutdown();
+}
+
+test "set_cursor" {
+    try init();
+
+    try set_cursor(15, 15);
+
+    try shutdown();
+}
+
+test "hide_cursor" {
+    try init();
+
+    try hide_cursor();
+
+    try shutdown();
+}
+
+test "print" {
+    try init();
+
+    try print(0, 0, .default, .default, "test");
+
+    try shutdown();
+}
+
+test "poll_event" {
+    try init();
+
+    const event = try poll_event();
+    std.debug.print("{}", .{event});
+
+    try shutdown();
+
+    return error.Test;
 }
